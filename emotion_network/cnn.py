7af54dcba783a2d0ad654b29f349a8a5bdc96aa7
nnamd <nwagwun@oregonstate.edu>
2023-11-10 10:28:38 -0800

    Notes for CNN implementation

---
+++ emotion_network/cnn.py
@@ -0,0 +1,7 @@
+#Ekman emotions: Happiness, Suprise, Sadness, Anger, Disgust, Fear
+#just CNN or CNN + BLSTM + Attention 
+#Dataset: https://www.kaggle.com/datasets/uwrfkaggler/ravdess-emotional-speech-audio
+#Ref paper: https://arxiv.org/ftp/arxiv/papers/2204/2204.13601.pdf 
+#More github ref: https://github.com/Data-Science-kosta/Speech-Emotion-Classification-with-PyTorch/blob/master/notebooks/parallel_cnn_attention_lstm.ipynb
+#Mel Spectrogram extraction: https://www.youtube.com/watch?v=TdnVE5m3o_0
+#Youtube tutorial set on CNN for audio: https://www.youtube.com/playlist?list=PL-wATfeyAMNoirN4idjev6aRu8ISZYVWm
